9d7741dbf3973fe3379e58feaf6a69d7955a9675
Shikhar Saxena <56793913+shikhar-saxena@users.noreply.github.com>
2021-04-17 22:13:09 +0530

    Some changes

--- dbConfig.js
+++ dbConfig.js
@@ -1,13 +1,14 @@
-// require("dotenv").config();
+require('dotenv').config({ path: './config/config.env'});
 
-// const { Pool } = require("pg");
+const { Pool } = require("pg");
 
-// const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 
-// const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-// const pool = new Pool({
-//   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-//   ssl: isProduction
-// });
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction
+});
 
+module.exports = {pool};
