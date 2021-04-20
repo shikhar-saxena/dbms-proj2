01703c1465b30c4567a89faf24fbfd5337910cc3
Shikhar Saxena <56793913+shikhar-saxena@users.noreply.github.com>
2021-04-20 13:10:04 +0530

    some change

--- app.js
+++ app.js
@@ -539,10 +539,10 @@ function checkAuthenticated(req, res, next) {
 }
 
 function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
+  if (!req.isAuthenticated()) {
+    return res.redirect("/users/login");  
   }
-  res.redirect("/users/login");
+  next();
 }
 
 // error handler
